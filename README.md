02de2f01758233e42bf90a84d9b819f01cf4b06d
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-03-03 15:55:16 +0100

    Bugs fixed

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ A web TODO App
 ## Fix
 
 - Fix Animations
+- Fix getTodos function
 
-## Add Soon
+## Tasks
 
 - Add functions to todo buttons
 - Add animations on create/delete todo
+- Store tasks in array and localStorage
--- public/script.js
+++ public/script.js
@@ -1,6 +1,10 @@
-//Add new TODO
 const newTodoForm = document.querySelector('form')
-const todoList = document.querySelector('.todo-list')
+const todoListElement = document.querySelector('.todo-list')
+const todoList = []
+const doneButton = document.querySelector('#done-button')
+const deleteButton = document.querySelector('#delete-button')
+//Add new TODO
+
 newTodoForm.addEventListener('submit', (event) => {
     const newTodoData = new FormData(newTodoForm)
 
@@ -14,7 +18,7 @@ newTodoForm.addEventListener('submit', (event) => {
     if (newTodo.todoName == '' || newTodo.todoDescription == '') {
         console.error('You must type something')
     } else {
-        todoList.innerHTML += `
+        todoListElement.innerHTML += `
         <div id="todo-card">
         <h3>${newTodo.todoName}</h3>
         <p>${newTodo.todoDescription}</p>
@@ -24,8 +28,26 @@ newTodoForm.addEventListener('submit', (event) => {
         </div>
         </div>
         `
-        console.log(JSON.stringify(newTodo))
+        todoList.push(newTodo)
+        localStorage.setItem('TodoArray', JSON.stringify(todoList))
     }
     newTodoForm.reset()
     event.preventDefault()
 })
+//Get stored todos
+const getTodos = () => {
+    if (localStorage != null) {
+        const storedTodos = JSON.stringify(localStorage.getItem('TodoArray'))
+        todoListElement.innerHTML += `
+        <div id="todo-card">
+        <h3>${storedTodos[0].todoName}</h3>
+        <p>${storedTodos[0].todoDescription}</p>
+        <div class="todo-buttons">
+        <button id="done-button"><i class="fas fa-check"></i></button>
+        <button id="delete-button"><i class="fas fa-times"></i></button>
+        </div>
+        </div>
+        `
+    }
+}
+//window.onload = getTodos()
